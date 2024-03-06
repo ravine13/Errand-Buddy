2c39568e50c02d9b678d9186ff32a3f8f2013f19
ravine13 <kuriaderek@gmail.com>
2024-03-06 16:58:24 +0300

    new: new routes

--- app/routes/users.py
+++ app/routes/users.py
@@ -2,13 +2,10 @@ from flask import Flask, Blueprint, jsonify, make_response, request
 from datetime import datetime
 from flask_restful import Api, Resource, reqparse
 from flask_marshmallow import Marshmallow
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema,auto_field
-from models import User,Message,Task,ErrandBoy,Payment,Rating,Category,Notification,db
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from models import User,db
 from marshmallow import Schema, fields
 from flask_jwt_extended import jwt_required
-from uuid import UUID
-from uuid import uuid4
-from datetime import datetime
 
 User_bp = Blueprint('user', __name__)
 api = Api(User_bp)
