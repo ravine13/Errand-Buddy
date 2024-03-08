fc37b4181cf3f2721bc88446b286bbd0ee2fc4cd
KELVIN MURITHI <kelvinmurithi@live.com>
2024-03-09 01:13:17 +0300

    Update link in SideNav

--- client/src/components/UserUI/SideNavUser.js
+++ client/src/components/UserUI/SideNavUser.js
@@ -35,7 +35,7 @@ const SideNavUser = () => {
       <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
       <li className="nav-item">
-          <Link to="/manager/manager_profile" className="nav-link">
+          <Link to="/user/profile" className="nav-link">
           <i className="nav-icon fas fa-user-circle" />
             <p>
               Profile
